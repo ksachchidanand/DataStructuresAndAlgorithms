4358c7bbd1708aeb78c6b7a137e6955a8e2dacd5
sdkr99 <sachchidanandx@gmail.com>
2021-07-01 15:17:05 +0530

    FastReader for fast io in java add

---
+++ src/io/FastReader.java
@@ -0,0 +1,40 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FastReader {
+    private final BufferedReader bufferedReader;
+    private StringTokenizer stringTokenizer;
+
+    public FastReader() {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String next() {
+        try {
+            while (stringTokenizer == null || !stringTokenizer.hasMoreElements()) {
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                ;
+            }
+        } catch (IOException ioException) {
+            System.out.println(ioException.getMessage());
+        }
+
+        return stringTokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
