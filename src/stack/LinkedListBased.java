a836815cf4d2c05620392681aa8cb2912be6cbca
sdkr99 <sachchidanandx@gmail.com>
2021-07-06 12:47:58 +0530

    Add stack wrapper over Linked List

---
+++ src/stack/LinkedListBased.java
@@ -0,0 +1,47 @@
+package stack;
+
+import java.util.NoSuchElementException;
+
+public class LinkedListBased<T> {
+    private class Node {
+        T item;
+        Node next;
+
+        Node(T item) {
+            this.item = item;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public void push(T item) {
+        var temp = head;
+        head = new Node(item);
+        head.next = temp;
+        size++;
+    }
+
+    public T pop() {
+        if (head == null)
+            throw new NoSuchElementException();
+        var temp = head;
+        head = head.next;
+        size--;
+        return temp.item;
+    }
+
+    public T peek() {
+        if (head == null)
+            throw new NoSuchElementException();
+        return head.item;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
